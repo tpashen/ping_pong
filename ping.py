1718d80f91ef5515f760bcddb2c942c07e11f4b1
tpashen <81863630+tpashen@users.noreply.github.com>
2021-04-09 22:19:00 +0300

    Update ping.py
    2/0

--- ping.py
+++ ping.py
@@ -27,7 +27,14 @@ class Player(GameSprite):
            self.rect.y -= self.speed
        if keys[K_s] and self.rect.y < win_height - 80:
            self.rect.y += self.speed
-#флаги, отвечающие за состояние игры
+#игровая сцена:
+back = (200, 255, 255) #цвет фона (background)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+         #флаги, отвечающие за состояние игры
 game = True
 finish = False
 clock = time.Clock()
